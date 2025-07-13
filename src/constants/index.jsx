a6411680d06af807e7b6fd938bc62d63aa535ac3
Priyam <priyamk778@gmail.com>
2025-07-13 13:52:53 +0530

    Initial commit

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -13,7 +13,7 @@ import user5 from "../assets/profile-pictures/user5.jpg";
 import user6 from "../assets/profile-pictures/user6.jpg";
 
 export const navItems = [
-  { label: "Features", href: "home" },
+  { label: "Features", href: "features" },
   { label: "Workflow", href: "workflow" },
   { label: "Pricing", href: "pricing" },
   { label: "Testimonials", href: "testimonials" },
